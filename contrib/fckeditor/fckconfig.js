769436f35b5ace7e314076ad2002c5177bfc4a89
Geoff Lehr <emeraldimp@users.noreply.github.com>
2007-06-04 16:15:42 +0000

    Added Palatino and Georgia to fontlist

--- contrib/fckeditor/fckconfig.js
+++ contrib/fckeditor/fckconfig.js
@@ -120,7 +120,7 @@ FCKConfig.ContextMenu = ['Generic','Table','Link','Anchor','BulletedList','Numbe
 
 FCKConfig.FontColors = '000000,993300,333300,003300,003366,000080,333399,333333,800000,FF6600,808000,808080,008080,0000FF,666699,808080,FF0000,FF9900,99CC00,339966,33CCCC,3366FF,800080,999999,FF00FF,FFCC00,FFFF00,00FF00,00FFFF,00CCFF,993366,C0C0C0,FF99CC,FFCC99,FFFF99,CCFFCC,CCFFFF,99CCFF,CC99FF,FFFFFF' ;
 
-FCKConfig.FontNames		= 'Arial;Comic Sans MS;Courier New;Tahoma;Times New Roman;Verdana' ;
+FCKConfig.FontNames		= 'Arial;Comic Sans MS;Courier New;Georgia;Palatino;Tahoma;Times New Roman;Verdana' ;
 FCKConfig.FontSizes		= '1/xx-small;2/x-small;3/small;4/medium;5/large;6/x-large;7/xx-large' ;
 FCKConfig.FontFormats	= 'p;div;pre;address;h1;h2;h3;h4;h5;h6' ;
 
